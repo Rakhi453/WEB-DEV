4aac930c945075f76083517fcf2f66c5111081d7
Rakhi453 <rakhee9521@gmail.com>
2022-03-05 22:15:51 +0530

    File created

--- Module 2/prime.js
+++ Module 2/prime.js
@@ -1,1 +1,17 @@
-let n
+let n=13;
+let flag=0
+for(let i=2; i*i<=n;i++){
+    if(n%i == 0){
+        flag=1;
+        break
+        console.log("not prime")
+    }
+    
+
+}    if(flag==1){
+    console.log('not prime')
+}
+else{
+
+    console.log("prime number");
+}   
