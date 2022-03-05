feaabca01ae5ecd9e2ea3a4fec6d3d7c2e3ea3e4
Rakhi453 <rakhee9521@gmail.com>
2022-03-05 22:41:48 +0530

    file created

---
+++ Module 2/function.js
@@ -0,0 +1,27 @@
+//function in js without parameter and return 
+function sayHello(){
+    console.log("Hello to functions");
+}
+sayHello();
+
+//function with parameter
+function sum(num1, num2){
+let addition=num1 +num2;
+    console.log("Addition of given numbers "+addition)
+}
+
+sum(5,6);
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+let ans=multiply(6,7);
+console.log(ans);
+
+
+//storing of function-function are first class citizen
+let a= function(num1, num2){
+    return num1-num2;
+}
+console.log(a(10,8));
+console.log(a);
