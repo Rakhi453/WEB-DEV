87975db815cf3b4f5b84ee0ca1d7625f423350d0
Rakhi453 <rakhee9521@gmail.com>
2022-03-05 21:58:05 +0530

    File created

---
+++ Module 2/intro.js
@@ -0,0 +1,20 @@
+//printing in javascript
+console.log("swagat hai aapka");
+
+//variables in javascript
+let a=10;
+console.log(a);
+
+let char='a';
+console.log(char);
+
+let str="I am a string";
+console.log(str);
+
+let bool=true;
+console.log(bool);
+
+//loop in javascript
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
