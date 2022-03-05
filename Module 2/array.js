396476e0a62e0d47664e71e6b9b180b221b68aaf
Rakhi453 <rakhee9521@gmail.com>
2022-03-05 23:29:06 +0530

    array created

--- Module 2/array.js
+++ Module 2/array.js
@@ -6,5 +6,25 @@ console.log(arr);
 
 console.log(arr);
 
-arr.push("new item")
+arr.push("new item1")
+arr.push("new item2")
+arr.push("new item3")
+
+console.log(arr);
+
+//2. pop
+console.log(arr);
+arr.pop();
+console.log(arr);
+
+//3. Array before shift- remove element from start
+
+console.log(arr);
+arr.shift();
+console.log(arr);
+
+//4. unshift- adds element in start of array
+
+console.log(arr);
+arr.unshift("newly added item");
 console.log(arr);
