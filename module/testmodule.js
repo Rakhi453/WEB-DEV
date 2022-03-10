d7978cf5e629d99b7fc0788b19b6e1fdd606b05c
Rakhi453 <rakhee9521@gmail.com>
2022-03-11 00:00:33 +0530

    crud complete

---
+++ module/testmodule.js
@@ -0,0 +1,7 @@
+let calc= require("./calculator.js");
+//console.log(calc);
+
+console.log(calc.addition(4,6));
+console.log(calc.subtraction(8,6));
+console.log(calc.multiply(9,7));
+//console.log(calc,divison(4,2));
