e247ece28bdc3792350e94945d156ba601d3b5fd
Rakhi453 <rakhee9521@gmail.com>
2022-03-10 21:49:47 +0530

    node modules

--- Module 2/node/childprocess.js
+++ Module 2/node/childprocess.js
@@ -3,6 +3,7 @@
 let cp= require('child_process')
 console.log(cp);
 cp.execFileSync("Calc");
-
+//execsync- executes synchronnously whst we write in it, it runs this command and give output
 let output= cp.execSync("node test.js");
-console.log("output is"+output);
+console.log("output is: "+output);
+//it gives data in binary form, so we convert it into string
