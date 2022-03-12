5ce1886ce429e0e7612d1f8c35dc6a215aa2861e
Rakhi453 <rakhee9521@gmail.com>
2022-03-12 23:43:23 +0530

    copy path

--- Module 2/node/file.js
+++ Module 2/node/file.js
@@ -22,3 +22,37 @@ console.log(content);
 //delete
 fs.unlinkSync(filePath);
 
+//create a directory
+//fs.mkdirSync("my directory");
+
+
+
+//to check if direactory exists
+if(!fs.existsSync("hmari directory")){
+    fs.mkdirSync("hmari directory");
+}
+
+//to read a directory(we gave here double slash to remove error)
+let folderPath="C:\\Users\\rakhi_000\\OneDrive\\Desktop\\FJP-6\\Module 1"
+let contentOfFolder=fs.readdirSync(folderPath);
+console.log(contentOfFolder);
+
+
+//to copy a file
+
+//let copyFile=fs.copyFileSync( C:\\Users\\rakhi_000\\OneDrive\\Desktop\\FJP-6\\module\\calculator.js, C:\\Users\\rakhi_000\\OneDrive\\Desktop\\FJP-6\\Module 2\\node );
+//console.log(copyFile);
+
+//delete a directory
+
+//fs.rmdirSync("hmari directory");
+
+
+//copy a file
+
+let sourcePath= path.join(__dirname, "file1.txt");
+let destPath= path.join(__dirname, "my directory", "file1.txt");
+console.log(sourcePath);
+console.log(destPath);
+
+fs.copyFileSync(sourcePath, destPath);
